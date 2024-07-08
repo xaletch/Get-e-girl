b480a983f90ce287fb8a36445bac670af0303e63
Xaletch <kiril.kolesnikov0609@mail.ru>
2024-07-08 15:54:02 +0300

    page profile

---
+++ app/modal.js
@@ -0,0 +1,27 @@
+const modal = document.querySelector(".modal");
+const menu = document
+  .querySelector(".modal__menu")
+  .addEventListener("click", (e) => e.stopPropagation());
+const buttonOpen = document.querySelector(".button-modal");
+
+const buttonApply = document.querySelector(".modal__button-apply");
+const buttonClose = document.querySelector(".modal__button-close");
+
+modal.addEventListener("click", closeMenu);
+buttonOpen.addEventListener("click", openMenu);
+buttonApply.addEventListener("click", applyChanges);
+buttonClose.addEventListener("click", closeMenu);
+
+function applyChanges() {
+  openMenu();
+  closeMenu();
+  console.log("Выход с аккаунта");
+}
+
+function openMenu() {
+  modal.classList.add("active");
+}
+
+function closeMenu() {
+  modal.classList.remove("active");
+}
